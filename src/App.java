5b2d1b41512ee258ee7a06a2b7b86e9265355605
= <=>
2024-09-29 11:30:58 -0600

    Start

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+		System.out.println("Hello, World!");
+		System.out.println("\033[H\033[2J"); // limpiar pantalla
+		System.out.println("Bye, World!");		
+		
+    }
+}
