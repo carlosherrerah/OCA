1ac9418db3e485ef03dfd3791afe4ca552415199
carlosherrerah <carlos.herrera@upa.edu.mx>
2024-10-12 08:26:26 -0600

    c1

--- src/scjp/c2/P3Overriding.java
+++ src/scjp/c2/P3Overriding.java
@@ -1,14 +1,15 @@
 package scjp.c2;
 
+import java.io.FileNotFoundException;
 
 class X {}
 class Y extends X {}
 
-class Base{
-  public X getValue() throws Exception { return null; }                //byte 1 overridden
+class A {
+  protected X /*int*/ getValue() throws Exception { return null; }            // overridden
 }
-class Base2 extends Base  {
-  public Y getValue() /* throws FileNotFoundException */  { return null; }  //byte 2 overriding
+class B extends A  {
+  public Y /*int*/ getValue()  throws FileNotFoundException { return null; }  // overriding
 }
 
 public class P3Overriding {
